662cdd17e4040936f8e5ff37cd1316e60824743b
Selma Deac <selmagoga@gmail.com>
2020-12-02 15:21:36 +0200

    Update BinFileReader.h

--- BinFileReader.h
+++ BinFileReader.h
@@ -5,28 +5,12 @@
 using namespace std;
 
 #define NR_0s_FILE_TITLE 10
-#define FOLDER "G:/Selma/dataset/2011_09_26/2011_09_26_drive_0005_sync/velodyne_points/data/"
 
 class BinFileReader{
 public:
 
-	void getCloud(int number, pcl::PointCloud<PointCustom> &my) {
-		int aux_n = number; //get nr cifre
-		int nrDigits = 0;
-		while (aux_n != 0) {
-			nrDigits++;
-			aux_n /= 10;
-		}
-		int nr0s = NR_0s_FILE_TITLE - nrDigits;
-		std::string _0s = "0";
-		std::string filename = FOLDER;
-		for (int i = 1; i < nr0s; i++) {
-			_0s += "0";
-		}
-		filename += _0s;
-		if (number != 0)
-			filename += std::to_string(number);
-		filename += ".bin";
+	void getCloud(std::string filepath, pcl::PointCloud<PointCustom> &my_cloud) {
+		std::string filename = filepath
 
 
 		float *velodyneData = new float[1000000];
@@ -36,15 +20,14 @@ public:
 
 		my.clear();
 		for (int i = 0; i < num; i++) {
-
 			PointCustom auxiliar = PointCustom();
 
 			auxiliar.intensity = velodyneData[i * 4 + 3];
-			auxiliar.x = velodyneData[i*4+0];
+			auxiliar.x = velodyneData[i*4 + 0];
 			auxiliar.y = velodyneData[i*4 + 1];
 			auxiliar.z = velodyneData[i*4 + 2];
 
-			my.push_back(auxiliar);
+			my_cloud.push_back(auxiliar);
 		}
 		delete[] velodyneData;
 	}
